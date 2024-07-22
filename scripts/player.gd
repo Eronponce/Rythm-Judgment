e1095ddef3e02b7f5fa7aceb86c9990a67a6edb0
Eron <81422635+Eronponce@users.noreply.github.com>
2024-07-21 23:11:29 -0300

    adicionado inimigo, gema com animaçoezinhas
    op inimigo somente move

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
-
+@onready var sprite = $AnimatedSprite2D
 func _physics_process(delta):
 	# Get the input direction for both horizontal and vertical movement
 	var direction = Vector2(
@@ -11,6 +11,12 @@ func _physics_process(delta):
 	
 	# Calculate velocity based on the direction and speed
 	velocity = direction * SPEED
-
+	
 	# Move the character and slide along collisions
 	move_and_slide()
+
+	# Flip the character based on the direction
+	if direction.x >0:
+		sprite.flip_h = true
+	elif direction.x <0:
+		sprite.flip_h = false
